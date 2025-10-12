e42835eb31cfded5d93b32450dc3c6a993c45835
Attila Toth <atoth@atoth.sote.hu>
2025-10-12 22:04:00 +0200

    initial commit: NGBS iCON integration
    initial commit: Home Assistant NGBS iCON integration

---
+++ custom_components/ngbs-icon/__init__.py
@@ -0,0 +1,28 @@
+import asyncio
+import aiohttp
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN, SESSION_REFRESH_INTERVAL
+
+async def async_setup_entry(hass: HomeAssistant, config_entry):
+    from .ngbs_icon_api import NGBSiConClient
+
+    username = config_entry.data["username"]
+    password = config_entry.data["password"]
+    icon_id = config_entry.data["icon_id"]
+    scan_interval = config_entry.options.get("scan_interval", 30)
+    session = aiohttp.ClientSession()
+    client = NGBSiConClient(session, username, password, icon_id)
+    hass.data.setdefault(DOMAIN, {})["client"] = client
+    hass.data[DOMAIN]["scan_interval"] = scan_interval
+
+    # Start periodic session refresh
+    async def periodic_refresh():
+        while True:
+            await client.async_login()
+            await asyncio.sleep(SESSION_REFRESH_INTERVAL)
+    hass.async_create_task(periodic_refresh())
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "climate")
+    )
+    return True
---
+++ custom_components/ngbs-icon/const.py
@@ -0,0 +1,4 @@
+DOMAIN = "ngbs_icon"
+PLATFORMS = ["climate"]
+DEFAULT_SCAN_INTERVAL = 30  # seconds
+SESSION_REFRESH_INTERVAL = 3600  # seconds
